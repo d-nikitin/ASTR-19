9492b511c702d5327c70a03cc87f6ce9d28509e7
Dmitriy Nikitin <60464540+d-nikitin@users.noreply.github.com>
2022-04-06 19:59:10 -0700

    Add files via upload

---
+++ w1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  30 19:54:29 2022
+
+@author: Dima
+"""
+
+def main():
+    print('My name is Dmitriy Nikitin.')
+    print('Pronouns: He/Him.')
+    print('My favorite movie is Watchmen which is a distopiant superhero flick.')
+
+    
+if __name__ == "__main__":
+    main()
