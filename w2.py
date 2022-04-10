21b289ec6bb923d12d36b027b6fc8db2ff48ed9a
Dmitriy Nikitin <60464540+d-nikitin@users.noreply.github.com>
2022-04-09 20:29:35 -0700

    Add files via upload

---
+++ w2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  9 20:24:50 2022
+
+@author: Dima
+"""
+
+def main():
+    x = 4.67 + 3.87
+    y = 69 - 42
+    z = 12 * 5.7
+    print(x)
+    print(type(x))
+    print(y)
+    print(type(y))
+    print(z)
+    print(type(z))
+    
+    
+if __name__ == "__main__":
+    main()
