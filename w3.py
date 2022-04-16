e3d9397d0f53c77f5b9825b51439aa9928f7e810
Dmitriy Nikitin <60464540+d-nikitin@users.noreply.github.com>
2022-04-16 07:26:06 -0700

    Add files via upload

---
+++ w3.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 16 07:23:11 2022
+
+@author: Dima
+"""
+
+def f(x):
+    return x**3 + 8
+
+def main():
+    x = 9
+    if f(x) > 27:
+        print('YAY!')
+if __name__ == "__main__":
+    main()
